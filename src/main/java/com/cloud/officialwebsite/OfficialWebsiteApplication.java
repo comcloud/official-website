065284d2032ecb11f19ddd5744334af5f16f91c4
亲爱的树树 <2230817302@qq.com>
2020-12-13 16:34:28 +0800

    第二版

---
+++ src/main/java/com/cloud/officialwebsite/OfficialWebsiteApplication.java
@@ -0,0 +1,13 @@
+package com.cloud.officialwebsite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OfficialWebsiteApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(OfficialWebsiteApplication.class, args);
+    }
+
+}
---
+++ src/main/java/com/cloud/officialwebsite/controller/IndexController.java
@@ -0,0 +1,45 @@
+package com.cloud.officialwebsite.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+/**
+ * @author 成都犀牛
+ * @version version 1.0
+ * @date 2020/12/13 11:03
+ */
+@Controller
+public class IndexController {
+
+    @RequestMapping(value ={"/" ,"/index"})
+    public String goToOfficialWeb(){
+        return "official-website/bl-first-index";
+    }
+    @RequestMapping(value = "/list")
+    public String goToList(){
+        return "official-website/bl-aritical-list";
+    }
+
+    @RequestMapping(value = "/compli")
+    public String goCompli(){
+        return "official-website/ydx-compli";
+    }
+    @RequestMapping(value = "/coop")
+    public String goToCoop(){
+        return "official-website/ydx-coop";
+    }
+    @RequestMapping(value = "/foundation")
+    public String goToFoundation(){
+        return "official-website/dev-foundation";
+    }
+    @RequestMapping(value = "/about")
+    public String goAbout1(){
+        return "official-website/bl-about";
+    }
+    @RequestMapping(value = "/about2")
+    public String goToAbout2(){
+        return "official-website/bl-about2";
+    }
+
+}
